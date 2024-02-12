ead7f26ea30b685a3ea99cbf3f48f019fd2e82b6
Rakeda <brandonmassie32@gmail.com>
2024-02-11 21:52:51 -0800

    Initial commit

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -23,7 +23,7 @@ export function App() {
 						switch(route.name){
 							case "home": return <Home />;
 							case "pageExample": return <PageExample />;
-							default : return <FourOhFour />;
+							default : return <Home />;
 						}
 					})()
 				}
