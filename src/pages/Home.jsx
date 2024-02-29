70ed7a3300ef8524e399cbee6fe7e1ab845ab76a
Rakeda <brandonmassie32@gmail.com>
2024-02-29 08:24:44 -0800

    modified:   src/pages/Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -23,6 +23,21 @@ export const Home = memo(() => {
 			/>
 
 			<GlArticle
+				className="article"
+				title={"Optimize Your Ecosystem With Sera"}
+				body={
+					"Sera leverages your existing ecosystem to enhance security efficiency and decision-making, integrating seamlessly with automation and custom tool development for a streamlined, adaptable security posture."}
+				illustration={{
+					"type": "custom component",
+					"Component": () => {
+						return (
+							<HomeFlow />
+						);
+					},
+				}}
+			/>
+
+			<GlArticle
 				className="article-right"
 				title={"Optimize Your Ecosystem With Sera"}
 				body={
